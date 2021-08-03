c8574f1e1a3600470f7346c77c9f865f46189f1b
Junfeng Zhang <jeffzx15o@gmail.com>
2021-08-03 17:10:47 +0800

    Remove resume store.

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -3,19 +3,16 @@ import { getModule } from "vuex-module-decorators";
 import BlogStore from "~/store/blog-store";
 import BlogListStore from "~/store/blog-list-store";
 import PureMDFileStore from "~/store/pure-md-file-store";
-import ResumeStore from "~/store/resume-store";
 
 /* eslint-disable import/no-mutable-exports */
 let blogStore: BlogStore;
 let blogListStore: BlogListStore;
 let pureMDFileStore: PureMDFileStore;
-let resumeStore: ResumeStore;
 
 function typedStore(store: Store<any>): void {
   pureMDFileStore = getModule(PureMDFileStore, store);
   blogListStore = getModule(BlogListStore, store);
   blogStore = getModule(BlogStore, store);
-  resumeStore = getModule(ResumeStore, store);
 }
 
-export { typedStore, blogListStore, blogStore, pureMDFileStore, resumeStore };
+export { typedStore, blogListStore, blogStore, pureMDFileStore };
