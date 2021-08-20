e12a8dcdebc21bc1ee8dcce9e93f71f88f5ff0ed
Junfeng Zhang <jeffzx15o@gmail.com>
2021-08-20 21:07:27 +0800

    feat: activity indicator view

---
+++ src/components/Loading.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="mtl-loading">
+    <div class="loader">
+      <svg viewBox="0 0 80 80">
+        <defs>
+          <linearGradient id="gradient" x1=".004%" x2="100.131%" y1="49.993%" y2="49.993%">
+            <stop offset="0%" stop-color="#6500FF"></stop>
+            <stop offset="16%" stop-color="#6A09FF"></stop>
+            <stop offset="43%" stop-color="#7623FF"></stop>
+            <stop offset="77%" stop-color="#8A4CFF"></stop>
+            <stop offset="99%" stop-color="#996BFF"></stop>
+          </linearGradient>
+        </defs>
+        <circle cx="40" cy="40" r="32" stroke="url(#gradient)"></circle>
+      </svg>
+    </div>
+    <div class="loader triangle">
+      <svg viewBox="0 0 86 80">
+        <polygon points="43 8 79 72 7 72" stroke="url(#gradient)"></polygon>
+      </svg>
+    </div>
+    <div class="loader">
+      <svg viewBox="0 0 80 80">
+        <rect x="8" y="8" width="64" height="64" stroke="url(#gradient)"></rect>
+      </svg>
+    </div>
+    <div class="description">Loading...</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { App, defineComponent } from "vue"
+
+var Loading = defineComponent({
+  name: "VLoading"
+})
+Loading.install = function (app: App) {
+  app.component(Loading.name, Loading)
+  return app
+}
+export default Loading
+</script>
+
+<style lang="less" scoped>
+.mtl-loading {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  z-index: 9999;
+  margin-top: -45px;
+  margin-left: -116px;
+
+  .loader {
+    --duration: 3s;
+    width: 44px;
+    height: 44px;
+    position: relative;
+    display: inline-block;
+    margin: 0 16px;
+
+    &::before {
+      content: " ";
+      width: 6px;
+      height: 6px;
+      border-radius: 50%;
+      position: absolute;
+      display: block;
+      background: #ff700a;
+      top: 37px;
+      left: 19px;
+      transform: translate(-18px, -18px);
+      animation: dotRect var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+    }
+
+    svg {
+      display: block;
+      width: 100%;
+      height: 100%;
+
+      circle {
+        stroke-dasharray: 150 50 150 50;
+        stroke-dashoffset: 75;
+        animation: pathCircle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+      }
+
+      polygon {
+        stroke-dasharray: 145 76 145 76;
+        stroke-dashoffset: 0;
+        animation: pathTriangle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+      }
+
+      rect {
+        stroke-dasharray: 192 64 192 64;
+        stroke-dashoffset: 0;
+        animation: pathRect 3s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+      }
+
+      circle,
+      polygon,
+      rect {
+        fill: none;
+        stroke-width: 10px;
+        stroke-linejoin: round;
+        stroke-linecap: round;
+      }
+    }
+
+    &.triangle {
+      width: 48px;
+
+      &::before {
+        left: 21px;
+        transform: translate(-10px, -18px);
+        animation: dotTriangle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+      }
+    }
+  }
+
+  .description {
+    text-align: center;
+    line-height: 40px;
+    font-size: 12px;
+    letter-spacing: 0.1em;
+    color: #666;
+  }
+}
+
+@keyframes pathTriangle {
+  33% {
+    stroke-dashoffset: 74;
+  }
+  66% {
+    stroke-dashoffset: 147;
+  }
+  to {
+    stroke-dashoffset: 221;
+  }
+}
+@keyframes dotTriangle {
+  33% {
+    transform: translate(0);
+  }
+  66% {
+    transform: translate(10px, -18px);
+  }
+  to {
+    transform: translate(-10px, -18px);
+  }
+}
+@keyframes pathRect {
+  25% {
+    stroke-dashoffset: 64;
+  }
+  50% {
+    stroke-dashoffset: 128;
+  }
+  75% {
+    stroke-dashoffset: 192;
+  }
+  to {
+    stroke-dashoffset: 256;
+  }
+}
+@keyframes dotRect {
+  25% {
+    transform: translate(0);
+  }
+  50% {
+    transform: translate(18px, -18px);
+  }
+  75% {
+    transform: translateY(-36px);
+  }
+  to {
+    transform: translate(-18px, -18px);
+  }
+}
+@keyframes pathCircle {
+  25% {
+    stroke-dashoffset: 125;
+  }
+  50% {
+    stroke-dashoffset: 175;
+  }
+  75% {
+    stroke-dashoffset: 225;
+  }
+  to {
+    stroke-dashoffset: 275;
+  }
+}
+</style>
