14c86aacefeba88e1e2daf72215480b9282466d0
Junfeng Zhang <jeffzx15o@gmail.com>
2021-08-20 14:07:50 +0800

    refactor: wrap models with namespace fluent

--- src/types/article.ts
+++
@@ -1,6 +0,0 @@
-import { TileAttributes } from "./tile-attributes"
-
-export interface Article extends TileAttributes {
-  excerpt?: string
-  content?: string
-}
--- src/types/tile-attributes.ts
+++
@@ -1,9 +0,0 @@
-/*
-    This interface is use to fill BlogTileView.
-*/
-export interface TileAttributes {
-  media?: string
-  tags?: Array<string>
-  headline?: string
-  timestamp?: string
-}
