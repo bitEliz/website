88a5ff873273919c8ce41c3ba9c0e437cf2cbfff
Junfeng Zhang <jeffzx15o@gmail.com>
2022-03-21 18:22:01 +0800

    chore: change md file render route

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,29 +2,18 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 
 const routes: RouteRecordRaw[] = [
   {
-    path: "/blog",
-    component: () => import("../views/blog/Index.vue"),
-    name: "blog"
-  },
-  {
-    path: "/blog/:id",
-    component: () => import("../views/blog/_id.vue"),
-    name: "blog-id"
+    path: "/",
+    redirect: "/resume"
   },
   {
     path: "/pure-md-resume",
-    component: () => import("../views/static/PureMDFile.vue"),
+    component: () => import("../views/static/pure-md-file"),
     name: "pure-md-resume"
   },
   {
     path: "/resume",
     component: () => import("../views/Resume.vue"),
     name: "resume"
-  },
-  {
-    path: "/static/:id",
-    component: () => import("../views/static/PureMDFile.vue"),
-    name: "static-id"
   }
 ]
 
