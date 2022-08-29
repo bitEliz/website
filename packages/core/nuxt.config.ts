3c4745b47ce10f491e99f344dcf106b0231d1eb8
Junfeng Zhang <jeffzx15o@gmail.com>
2022-08-30 01:06:58 +0800

    chore: clean workspace

--- packages/core/nuxt.config.ts
+++ packages/core/nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    buildAssetsDir: 'assets'
+    buildAssetsDir: 'assets/'
   },
   css: ['bootstrap/scss/bootstrap.scss'],
   srcDir: 'src',
