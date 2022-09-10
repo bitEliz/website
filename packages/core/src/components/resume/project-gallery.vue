d617ecd6f4c7d16ef17fcdf53570c88a0ca291a7
Junfeng Zhang <jeffzx15o@gmail.com>
2022-09-11 00:30:39 +0800

    refactor: replace project galery item with tile

--- packages/core/src/components/resume/project-gallery.vue
+++ packages/core/src/components/resume/project-gallery.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="grid" style="--bs-gap: 1rem" v-for="g in galleries">
     <div class="g-col-12 g-col-md-6 g-col-lg-4" v-for="e in g">
-      <a
-        v-if="e.trackViewUrl"
-        class="text-decoration-none"
-        :href="e.trackViewUrl"
-        target="_blank"
-      >
-        <ResumeProjectGalleryItem :="e" />
+      <a :href="e.trackViewUrl" target="_blank">
+        <Tile
+          class="text-body border-light"
+          :data="{ title: e.name }"
+          aspect-ratio="6rem"
+        >
+          <template #media>
+            <div class="p-3 pb-0">
+              <SymbolsGithub v-if="e.isOpenSource && !e.artworkUrl" />
+              <img :src="e.artworkUrl" alt="" v-else />
+            </div>
+          </template>
+          <template #excerpt>
+            <Markdown class="tile-excerpt" :content="e.summary" />
+          </template>
+        </Tile>
       </a>
-      <ResumeProjectGalleryItem v-else :="e" />
     </div>
   </div>
 </template>
@@ -17,8 +25,7 @@
 <script setup lang="ts">
 import { Project } from '@/types/fluent'
 
-const props = defineProps<{ galleries: Array<Array<Project>> }>()
-const galleries = props.galleries
+const { galleries } = defineProps<{ galleries: Array<Array<Project>> }>()
 </script>
 
 <style lang="scss" scoped>
@@ -27,12 +34,11 @@ const galleries = props.galleries
     padding-bottom: var(--#{$prefix}gap);
   }
 
-  a {
-    color: var(--#{$prefix}body-color);
-
-    &:focus,
-    &:hover {
-      color: var(--#{$prefix}body-color);
+  .tile {
+    img,
+    svg {
+      height: 100%;
+      @include border-radius(1rem);
     }
   }
 }
