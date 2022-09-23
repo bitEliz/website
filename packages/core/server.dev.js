99818cf33d437adf0473939503812b74683dd912
Junfeng Zhang <jeffzx15o@gmail.com>
2022-09-23 21:44:56 +0800

    chore: split server into multiple file deps on env

--- packages/core/server.dev.js
+++ packages/core/server.dev.js
@@ -1,31 +1,40 @@
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import express from 'express'
+import { createServer } from 'vite'
 import manifest from './dist/client/ssr-manifest.json' assert { type: 'json' }
 ;(async () => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-  let template = fs.readFileSync(
-    path.resolve(__dirname, 'dist/client/index.html'),
-    'utf-8'
-  )
-
   const app = express()
 
-  app.use((await import('compression')).default())
-  app.use(
-    '/',
-    (await import('serve-static')).default(
-      path.resolve(__dirname, 'dist/client'),
-      {
-        index: false
+  const vite = await createServer({
+    logLevel: 'error',
+    server: {
+      middlewareMode: true,
+      watch: {
+        // During tests we edit the files too fast and sometimes chokidar
+        // misses change events, so enforce polling for consistency
+        usePolling: true,
+        interval: 100
       }
-    )
-  )
+    },
+    appType: 'custom'
+  })
+
+  // use vite's connect instance as middleware
+  app.use(vite.middlewares)
 
   app.use('*', async (context) => {
     try {
-      const renderer = await import('./dist/server/entry-server.js')
+      // always read fresh template in dev
+      let template = fs.readFileSync(
+        path.resolve(__dirname, 'index.html'),
+        'utf-8'
+      )
+      template = await vite.transformIndexHtml(context.originalUrl, template)
+      const renderer = await vite.ssrLoadModule('/src/entry-server.ts')
 
       const [appHtml, preloadLinks, state] = await renderer.render(
         context.originalUrl,
@@ -39,7 +48,8 @@ import manifest from './dist/client/ssr-manifest.json' assert { type: 'json' }
 
       context.res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
     } catch (e) {
-      console.error(e.stack)
+      vite.ssrFixStacktrace(e)
+      console.log(e.stack)
       context.res.status(500).end(e.stack)
     }
   })
